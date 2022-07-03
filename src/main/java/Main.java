770c23d448b8a7842266023ca04ecc571eb30627
Bartłomiej Gajewski <bartek.gajewski994@gmail.com>
2022-07-04 00:02:08 +0200

    MainExtended

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Maciej";
+        System.out.println(myName);
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+
+
+    }
+}
+
---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,19 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String carType = "Coupe";
+        String carBrand = "Porsche";
+        int power = 300;
+        int speed = 250;
+        char CarClass = 'S';
+        boolean isMadeInGerman = true;
+        System.out.println(carType);
+        System.out.println(carBrand);
+        System.out.println(power);
+        System.out.println(speed);
+        System.out.println(CarClass);
+        System.out.println(isMadeInGerman);
+
+    }
+
+}
+
