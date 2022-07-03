50b10cf473c363308dc03d08d7b122e24e01d6f7
Bartłomiej Gajewski <bartek.gajewski994@gmail.com>
2022-07-03 22:48:04 +0200

    HelloWorld

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Bartek");
+        System.out.println("27");
+        System.out.println("G");
+    }
+
+}
+
+
