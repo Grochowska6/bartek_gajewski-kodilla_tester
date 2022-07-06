404ab4faeb37fdd0110602656411e7851c2ad68a
Bartłomiej Gajewski <bartek.gajewski994@gmail.com>
2022-07-06 21:34:53 +0200

    LeapYears 1

---
+++ kodilla-intro/LeapYear.java
@@ -0,0 +1,15 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2022;
+        int forYear = 4;
+
+        System.out.println("year");
+        System.out.println("forYear");{
+
+        }
+
+
+    }
+
+}
+
