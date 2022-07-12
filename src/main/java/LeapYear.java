2389dc2b83902008822c282d9044170a86f06542
Bartłomiej Gajewski <bartek.gajewski994@gmail.com>
2022-07-12 19:55:54 +0200

    LeapYear 1.2

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,21 +1,13 @@
 public class LeapYear {
     public static void main(String[] args) {
-        int a = 2000;
+        int a = 2022;
         int b = 4;
         int c = 100;
         int d = 400;
 
 
         boolean aIsEven = a % b == 0;
-        System.out.println("liczba jest parzysta" + aIsEven);{
 
-        }
-        boolean result1 = a % b == 0; // true, gdy jest parzysta
-        boolean result2 = a % c == 0; // true, gdy jest parzysta
-        boolean result3 = a % d == 0; // true, gdy jest parzysta
-        System.out.println(result3);
-        System.out.println(result2);
-        System.out.println(result1);
 
         if (a % b <= 0){
             System.out.println("przestępny");
