df7415e3432df1c6efc9c6731ca546ddda99661b
Bartłomiej Gajewski <bartek.gajewski994@gmail.com>
2022-07-16 22:26:29 +0200

    stage 1.0

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,6 +1,39 @@
-public class FirstClass {                           // [1]
-    public static void main(String[] args) {         // [2]
-        System.out.println("Hello from FirstClass!"); // [3]
-        String example = "Hello form FirstClass";
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+
+        Notebook notebook = new Notebook(600,1000,2020);
+        System.out.println(notebook.weight + " " + notebook.price + " " + notebook.year);
+        notebook.CheckPrice();
+        notebook.CheckWeight();
+        notebook.CheckYear();
+
+        Notebook HeavyNotebook = new Notebook(2000,1500,2005);
+        System.out.println(HeavyNotebook.weight + " " + HeavyNotebook.price + " " + HeavyNotebook.year);
+        HeavyNotebook.CheckPrice();
+        HeavyNotebook.CheckWeight();
+        HeavyNotebook.CheckYear();
+
+        Notebook OldNotebook = new Notebook(1200,500,1999);
+        System.out.println(OldNotebook.weight + " " + OldNotebook.price + " " + OldNotebook.year);
+        OldNotebook.CheckPrice();
+        OldNotebook.CheckWeight();
+        OldNotebook.CheckYear();
+
+        Notebook FutureNotebook = new Notebook(567,9999,2023);
+        System.out.println(FutureNotebook.weight + " " + FutureNotebook.price + " " + FutureNotebook.year);
+        FutureNotebook.CheckPrice();
+        FutureNotebook.CheckWeight();
+        FutureNotebook.CheckYear();
+
+
+
+        }
+
+
     }
-}
+
+
+
+
---
+++ src/main/java/Notebook.java
@@ -0,0 +1,58 @@
+public class Notebook {
+
+    int weight;
+    int price;
+    int year;
+
+    public static void main(String[] args) {
+
+    }
+
+    public Notebook(int weight, int price, int year) {
+        this.weight = weight;
+        this.price = price;
+        this.year = year;
+
+
+    }
+
+    public void CheckPrice() {
+        if (this.price < 600) {
+            System.out.println("This notebook is very Cheap");
+        } else {
+        }
+        if (this.price >= 600 && this.price <= 1000) {
+            System.out.println("This price is good");
+        } else {
+        }
+        if (this.price > 1000 ) {
+            System.out.println("This notebook is expensive");
+        } else {
+        }
+    }
+    public void CheckWeight() {
+        if (this.weight <= 600) {
+            System.out.println("This notebook is light as a feather");
+        } else {
+        }
+        if (this.weight > 600 && this.weight < 1200) {
+            System.out.println("This notebook is passable");
+        } else {
+        }
+        if (this.weight >= 1200) {
+            System.out.println("This notebook is to heavy");
+        } else {
+        }
+    }
+    public void CheckYear() {
+        if (this.year > 2005 || this.price > 600) {
+            System.out.println("this notebook is old" + " and " + "This notebook is cheap");
+        } else {
+
+        }
+
+
+    }
+
+}
+
